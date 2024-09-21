45aa500a1068e16e7d9daad8a6e0d917e3779aff
William Huang <whlxgx@gmail.com>
2024-09-20 23:02:33 -0700

    Breaking down the url into segments

--- WebTester.py
+++ WebTester.py
@@ -4,14 +4,20 @@ import sys
 
 def web_tester(url):
     if url.startswith("https://"):
+        cut_off_length = len("https://")
         port = 443
     elif url.startswith("http://"):
+        cut_off_length = len("http://")
         port = 80
     else:
         print("Invalid URL format. Please use http:// or https://")
         return
     
-    
+    url_segments = url[cut_off_length: ].split("/", 1)
+    host = url_segments[0]
+    path = "/" + url_segments[1] if len(url_segments) > 1 else "/"
+
+
 
     
 
