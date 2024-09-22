cb5b0a17ea5c55d863b193ddc5eab5d328a7dfe7
William Huang <whlxgx@gmail.com>
2024-09-21 22:51:44 -0700

    sending GET request and receiving data using socket.recv()

--- WebTester.py
+++ WebTester.py
@@ -2,6 +2,31 @@ import socket
 import ssl
 import sys
 
+
+def sending_request(host, path, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+
+    if port == 443:
+        context = ssl.create_default_context()
+        s = context.wrap_socket(s, server_hostname=host)
+
+    request = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: Keep-Alive\r\n\r\n"
+    s.send(request.encode())
+
+    response = b''
+
+    while True:
+        data = s.recv(4096)
+        response += data
+        if not data:
+            s.close()
+            break
+
+    print(response)
+
+    return response
+
 def web_tester(url):
     if url.startswith("https://"):
         cut_off_length = len("https://")
@@ -10,12 +35,16 @@ def web_tester(url):
         cut_off_length = len("http://")
         port = 80
     else:
-        print("Invalid URL format. Please use http:// or https://")
+        print("The input format is not supported, please enter a URL with the https:// or https://")
         return
     
     url_segments = url[cut_off_length: ].split("/", 1)
     host = url_segments[0]
     path = "/" + url_segments[1] if len(url_segments) > 1 else "/"
+
+    response = sending_request(host, path, port)
+
+    print(response)
 
 
 
